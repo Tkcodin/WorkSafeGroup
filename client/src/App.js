f008860a2882252ddced3d30ba2b34613c1ba646
Sapan Sandesara <sapansan95@gmail.com>
2023-01-26 10:28:15 +1300

    Added read comment component

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,10 @@
-import logo from './logo.svg';
+
 import './App.css';
+import ReadComment from './components/readcomment.component';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React tom-adding-text-to-test
-          test guangkun
-          test again. test again!!!!!
-          test !!!!!!
-
-        </a>
-      </header>
-    </div>
-  );
+  
+  return;
 }
 
 export default App;
