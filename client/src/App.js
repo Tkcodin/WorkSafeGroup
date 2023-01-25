b6038f48da1c3ae70a2ea37d16f1227e9108136d
Tom <tkpaulrogers@gmail.com>
2023-01-26 10:03:20 +1300

    Updating with my work on tags

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import MyTag from './components-tom/MyTag/MyTag.js';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a>
           Learn React tom-adding-text-to-test
           test guangkun
           test again. test again!!!!!
           test !!!!!!
-
+          <MyTag text="violence2" colour="green"/>
+          <MyTag text="heights" colour="blue"/>
+          <MyTag text="Air Toxins" colour="purple"/>
         </a>
       </header>
+
+      
+
     </div>
   );
 }
