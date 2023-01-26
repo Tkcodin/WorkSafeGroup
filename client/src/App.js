3b79f6b0a5ea3b81ddaf08c7a59f8c2cebb78eb9
Tom <tkpaulrogers@gmail.com>
2023-01-26 13:41:43 +1300

    Adding textbox react component

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,9 @@ import logo from './logo.svg'
 import NavigationBar from './navigationBar';
 import MyTag from './components-tom/MyTag/MyTag.js';
 import MakeCommentComponent from "./MakeCommentCOmponent.jsx";
+import MyTextBox from './components-tom/MyTextBox/MyTextBox.js';
+
+
 
 function App() {
   return (
@@ -27,6 +30,10 @@ function App() {
           <MyTag text="Air Toxins" colour="purple"/>
         </a>
         <MakeCommentComponent> </MakeCommentComponent>
+
+        <MyTextBox prompt="type here: " height="50px" inst="Enter text Here"/>
+        <MyTextBox prompt="type here2: " height="200px" inst="Enter text Here2"/>
+
       </header>
 
       
