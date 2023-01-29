11b0a9f5ad861a75d247677bdd47da925a81199b
Tom <tkpaulrogers@gmail.com>
2023-01-30 11:16:40 +1300

    Adding nav bar basic style slight update to tags

--- client/src/components-tom/MyTag/MyTagContainer.js
+++ client/src/components-tom/MyTag/MyTagContainer.js
@@ -1,24 +1,26 @@
 import { useState } from 'react';
 import React from 'react';
+import MyTag from './MyTag.js';
 
 
 const MyTagContainer = (props) => {
 
     const [visible, setVisible] = useState(true);
     
-    // const removeTagContainer = () => {
-    //     setVisible((prevVisible) => !prevVisibile);
-    // }
+  
+    
 
-
+    
 
     return(
+        // indexChore(),
         visible && <div className="MyTagContainer">
             <React.Fragment>
                 {props.myTags}
             </React.Fragment>
         </div>
     )
+
 }
 
 export default MyTagContainer;
