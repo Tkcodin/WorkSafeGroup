b1aae311f95772792b155615911c23ba64b4f1e1
jiguan <jiguan@myvuw.ac.nz>
2023-02-01 15:09:41 +1300

    likes_0

--- client/src/StoryContent/StoryContent.js
+++ client/src/StoryContent/StoryContent.js
@@ -15,7 +15,8 @@ const StoryContent = () => {
   const [Image, setImage] = useState('');
   const[Tags,setTags] = useState('');
   const[Date,setDate]=useState('');
-
+  const [likes, setLikes] = useState(0);  // new
+  const [liked, setLiked] = useState(false); // new
   
     useEffect(() => {
       const id = objectId;
@@ -31,13 +32,28 @@ const StoryContent = () => {
         setImage(data.Image);
         setTags(data.tags);
         setDate(data.Date);
-        
+        setLikes(data.likes); // new
 
         })
       
       .catch(error => {console.log(error);console.log(objectId.id)});
     });
    
+    // new
+    const handleLike = () => {
+      setLiked(!liked);
+      if (liked) {
+        setLikes(likes - 1);
+      } else {
+        setLikes(likes + 1);
+      }
+      axios.post('http://localhost:3000/updatelikes/' + objectId.id, {
+        likes: likes
+      })
+        .then(res => console.log(res.data))
+        .catch(error => console.log(error));
+    };
+
     return (
       
 
@@ -59,6 +75,13 @@ const StoryContent = () => {
               </b>
             </span>
             <span>{Date}</span>
+            <span> // new
+              <i
+                className={`fa fa-heart${liked ? '' : '-o'}`}
+                onClick={handleLike}
+              />
+              {likes}
+            </span>
           </div>
           <p className="StoryContentDesc">
             {Content}
