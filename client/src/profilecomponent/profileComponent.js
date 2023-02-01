b8eff98b61534efcfd246fedc012366bfc386f0e
Sapan Sandesara <sapansan95@gmail.com>
2023-02-01 21:06:29 +1300

    Added profileComponent.

--- client/src/profilecomponent/profileComponent.js
+++ client/src/profilecomponent/profileComponent.js
@@ -2,17 +2,20 @@ import { useEffect, useState } from "react";
 import React from "react";
 import NavigationBar from "../navigationBar";
 import './profileComponent.css';
+import profilepicture from './profilepic.jpg';
 
 
 
 
 const ProfileComponent =()=>{
-    const  objectId  = useParams();
-    const [firstname, setFirstName] = useState('');
-    const [lastname, setLastName] = useState('');
-    const [Role, setRole] = useState('');
-    const [Employer, setEmployer] = useState('');
-    const [About, setAbout] = useState('');
+    //Once we have set up the backend we will use the url params to pass the object id and then use effect hook to grab the user details from DB.
+    //const  objectId  = useParams();
+    const [firstname, setFirstName] = useState('sapan');
+    const [lastname, setLastName] = useState('sandesara');
+    const [Role, setRole] = useState('Safety Expert');
+    const [Employer, setEmployer] = useState('Worksafe NZ Limited');
+    const [About, setAbout] = useState('I am a safety expert at worksafe limited. I am passionate about safety and spend my free time answering any questions you might have');
+    const [Interests, setInterests] = useState('Violence, Poison');
 
 
     useEffect(()=>{
@@ -21,8 +24,12 @@ const ProfileComponent =()=>{
 
     return (
 
-        <><></><NavigationBar /><div className="ProfileContent">
-            
+        <><></><NavigationBar /><div className="profileimage">
+            <img className="profilepic" src={profilepicture} alt="profilepic"></img>
+            <h1 className="Author">{firstname} {lastname}</h1><br></br>
+            <h2 classname="details">{Role} at {Employer}</h2><br></br>
+            <p className="About">About me: <br></br>{About}</p><br></br>
+            <p classname ="details">Interests: {Interests}</p>
             </div></>
 
 
