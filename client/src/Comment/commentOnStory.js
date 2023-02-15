7e1a184e4841b2aeeeac57fccd1d105ef3755e5a
tuteredurie <durietute@ecs.vuw.ac.nz>
2023-02-15 17:02:16 +1300

    UI changes to story content

--- client/src/Comment/commentOnStory.js
+++ client/src/Comment/commentOnStory.js
@@ -1,7 +1,8 @@
 import CommentTree from './Comment2Tree.js';
 import Comment from './Comment2';
-import './Comment2.css';
+// import './Comment2.css';
 import { useState, useEffect } from "react";
+import user from '/Users/tuteredurie/WorkSafeGroup/client/src/user.png';
 
 const CommentOnStory = (props) => {
 
@@ -11,7 +12,7 @@ var modal2 = document.getElementById('logInModal');
 function ReplyPost(e){
     e.preventDefault();
     if (localStorage.getItem('userID') !== null) {
-      let t = document.getElementById("c2RInput").value;
+      let t = document.getElementById("commentInput").value;
       props.addComment(t);
     } else {
       modal2.style.display ='block';
@@ -20,10 +21,25 @@ function ReplyPost(e){
 }
 
       return (
-        <div id="cos">
-            <input type="text" id="c2RInput"></input>
-            <button id="c2RPost" onClick={(e) => ReplyPost(e)}>Post</button>
-        </div>
+        // <div id="cos">
+        //     <input type="text" id="c2RInput"></input>
+        //     <button id="c2RPost" onClick={(e) => ReplyPost(e)}>Post</button>
+        // </div>
+
+        <div id="commentBox">
+                {/* <form onSubmit={this.onPost}> */}
+                    {/* <img src={user} alt="Icon" id='userIcon' /> */}
+                    <input type="text" placeholder='Write a comment...' id='commentInput'
+                    // value={this.state.comment}
+                    // onChange={this.onChangeComment}
+                    />
+                    <button type='submit' id='postCommentButton'
+                    onClick={(e) => ReplyPost(e)}
+                    >Post</button>
+                {/* </form> */}
+                
+            </div>
+        
       );
     }
   
